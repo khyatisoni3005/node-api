a0d58c3e81cb4d9c526334530a655be517f923e7
khyatisoni3005 <khyati.soni4035@gmail.com>
2024-01-25 11:25:46 +0530

    change update

---
+++ data.js
@@ -0,0 +1,12 @@
+const data = [
+    { name: "khyati soni", email: "khyati@123.com" },
+    { name: "harshil soni", email: "harshil@123.com" },
+    { name: "neel soni", email: "neel@123.com" },
+    { name: "ritik soni", email: "ritik@123.com" },
+    { name: "soni soni", email: "soni@123.com" },
+
+]
+
+module.exports = data
+
+
